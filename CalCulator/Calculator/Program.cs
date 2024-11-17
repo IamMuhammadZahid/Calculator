e17921b8b0dac8ea4740192cf9bd5e9841f9c4be
IamMuhammadZahid <mzahid1725@gmail.com>
2024-11-17 16:32:25 +0500

    Add Subtraction
    After Take two inputs from user, then user decide the option of sum and subtraction, if user choice sum then values show result of sum, if user choice subtraction then result of Subtract

--- CalCulator/Calculator/Program.cs
+++ CalCulator/Calculator/Program.cs
@@ -1,9 +1,28 @@
 ﻿int ValueOne;
 int ValueTwo;
 int Sum;
+int Subtract;
+int check = 0;
 Console.WriteLine("Enter The First Number: ");
 ValueOne = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Enter The Second Number: ");
 ValueTwo = Convert.ToInt32(Console.ReadLine());
-Sum = ValueOne + ValueTwo;
-Console.WriteLine("The Sum is: " + Sum);
+Console.WriteLine("Please Enter \n 1 For Sum \n 2 For Subtraction");
+
+check = Convert.ToInt32(Console.ReadLine());
+
+if (check == 1)
+{
+    Sum = ValueOne + ValueTwo;
+    Console.WriteLine("The Sum is: " + Sum);
+}
+
+else if (check == 2)
+{
+    Subtract = ValueOne - ValueTwo;
+    Console.WriteLine("The Subtract is: " + Subtract);
+}
+else
+{
+    Console.WriteLine("Invalid Input ");
+}
