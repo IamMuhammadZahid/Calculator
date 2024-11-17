616eaa9b768c34e011274ead934844ce29d763ae
IamMuhammadZahid <mzahid1725@gmail.com>
2024-11-17 18:23:16 +0500

    Calculatore With Do While Loop
    Using Do While loop create calculatore

--- CalCulator/Calculator/Program.cs
+++ CalCulator/Calculator/Program.cs
@@ -5,34 +5,45 @@ int Subtract;
 int Multiply;
 float Divide;
 int check;
-Console.WriteLine("Enter The First Number: ");
-ValueOne = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter The Second Number: ");
-ValueTwo = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Please Enter \n 1 For Sum \n 2 For Subtraction \n 3 For Multiplication \n 4 For Division");
 
-check = Convert.ToInt32(Console.ReadLine());
-
-switch (check)
+do
 {
-    case 1:
-        Sum = ValueOne + ValueTwo;
-        Console.WriteLine("The Sum is: " + Sum);
-        break;
-    case 2:
-        Subtract = ValueOne - ValueTwo;
-        Console.WriteLine("The Subtract is: " + Subtract);
-        break;
-    case 3:
-        Multiply = ValueOne * ValueTwo;
-        Console.WriteLine("The Multiply is: " + Multiply);
-        break;
-    case 4:
-        Divide = (float)ValueOne / ValueTwo;
-        Console.WriteLine("The Divide is: " + Divide);
-        break;
-    default:
-        Console.WriteLine("Invalid Input ");
-        break;
+    Console.WriteLine("Enter The First Number: ");
+    ValueOne = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Enter The Second Number: ");
+    ValueTwo = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Please Enter \n 1 For Sum \n 2 For Subtraction \n 3 For Multiplication \n 4 For Division");
+
+    check = Convert.ToInt32(Console.ReadLine());
+    
+    switch (check)
+    {
+        case 1:
+            Sum = ValueOne + ValueTwo;
+            Console.WriteLine("The Sum is: " + Sum);
+            break;
+        case 2:
+            Subtract = ValueOne - ValueTwo;
+            Console.WriteLine("The Subtract is: " + Subtract);
+            break;
+        case 3:
+            Multiply = ValueOne * ValueTwo;
+            Console.WriteLine("The Multiply is: " + Multiply);
+            break;
+        case 4:
+            Divide = (float)ValueOne / ValueTwo;
+            Console.WriteLine("The Divide is: " + Divide);
+            break;
+        default:
+            Console.WriteLine("Invalid Input ");
+            break;
         
-}
+    }    
+    
+    Console.WriteLine(" Press 0 To Continue, Any Key To Exit ");
+    check = Convert.ToInt32(Console.ReadLine());
+    
+} while (check == 0);
+
+
+
--- CalCulator/Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
+++ CalCulator/Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36d5512dad30497dbc9f267521fad0c4ac447531")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c61059e505bd9cb15d6c9093184b8b5fd0debd60")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
