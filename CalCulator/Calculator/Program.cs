c4e11b6e19319156c9b45e35a2c3fb6471fb7e99
IamMuhammadZahid <mzahid1725@gmail.com>
2024-11-17 16:09:33 +0500

    Simple Calculator
    Simple take two values from user and sum these values and print on screen

---
+++ CalCulator/Calculator/Program.cs
@@ -0,0 +1,9 @@
+﻿int ValueOne;
+int ValueTwo;
+int Sum;
+Console.WriteLine("Enter The First Number: ");
+ValueOne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter The Second Number: ");
+ValueTwo = Convert.ToInt32(Console.ReadLine());
+Sum = ValueOne + ValueTwo;
+Console.WriteLine("The Sum is: " + Sum);
